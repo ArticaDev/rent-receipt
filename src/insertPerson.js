bc32a664d5e0e8bd0d88baa44c9ec88af2f585f2
Marcos <marcosnery.comp@gmail.com>
2021-03-27 21:15:28 -0300

    feat: adding insert new person

---
+++ src/insertPerson.js
@@ -0,0 +1,22 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+$(function() {
+    $("#person-info-form").click(function() {
+
+        let value = $('#value').val()
+        value = value.split("R$ ")[1]
+
+        db.get('people')
+            .push({ name:$('#name').val(), value:value, 
+                    cpf:$('#cpf').val(), address:$('#address').val(), 
+                    subject:$('#subject').val(), day:$('#day').val(), 
+                    emitter:$('#emitter').val() 
+                })
+            .write()
+        
+    });
+
+});

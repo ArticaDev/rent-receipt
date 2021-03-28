d31673ff10f90cd107db2813675b46b9968e232b
Marcos <marcosnery.comp@gmail.com>
2021-03-27 21:23:53 -0300

    feat: displaying receipts in index table view

--- src/index.js
+++ src/index.js
@@ -1,62 +1,38 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ people: [] })
-  .write()
+function createPersonItem(Person) {
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
+    let $row = $(`<tr>
+        <td><a href="profile.html?name=${Person.name}">${Person.name}</a></td>
+        <td>${Person.subject}</td>
+        <td>${Person.address}</td>
+        <td>${Person.emitter}</td>
+        <td>${Person.day}</td>
+        <td>
+          <a class="btn btn-primary btn-sm d-none d-sm-inline-block"
+            role="button" href="receipts.html">
+            <i class="fas fa-download fa-sm text-white-50"></i>
+            Gerar Recibos</a>
+        </td>
+      </tr>`);
+
+    return $row;
+
 }
 
-const createWindow = () => {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true
-    }
-   }
-  );
+function updateView() {
 
-  // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    people = db.get('people');
+    people.value().forEach(person => {
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-};
+        let $personItem = createPersonItem(person);
+        $('#people-table').append($personItem);
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+    });
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+updateView();

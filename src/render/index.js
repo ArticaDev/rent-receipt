4282aa71b324eb5776254f2d6bac8759e88840a2
Marcos <marcosnery.comp@gmail.com>
2021-03-31 10:42:17 -0300

    feat: implementing pag

--- src/render/index.js
+++ src/render/index.js
@@ -1,6 +1,22 @@
 const db = window.db;
+const querystring = require('querystring');
 
-function createPersonItem(Person) {
+
+$(function() {
+
+  let queryParams = querystring.parse(global.location.search);
+  let begin = $.isEmptyObject(queryParams)? 0 : queryParams['?begin'];
+  let numberToShow = $.isEmptyObject(queryParams)? 10 : queryParams['numberToShow'];
+
+  $("#number-to-show").val(numberToShow);
+
+  $('#number-to-show').change(function(){
+    let numberToShow = $('#number-to-show option:selected').val();
+    window.location.href=`index.html?begin=0&numberToShow=${numberToShow}`
+  })
+
+
+  function createPersonItem(Person) {
 
     let $row = $(`<tr>
         <td><a href="profile.html?name=${Person.name}">${Person.name}</a></td>
@@ -22,8 +38,37 @@ function createPersonItem(Person) {
 
 function updateView() {
 
-    people = db.get('people');
-    people.value().forEach(person => {
+
+      people = db.get('people').value(); 
+
+    
+    let numberOfRecords = people.length;
+
+    if(begin != 0){
+      
+      let $previous = $(`<li class="page-item ">
+      <a class="page-link" href="index.html?begin=${begin-numberToShow}&numberToShow=${numberToShow}" 
+      aria-label="Previous">
+      <span aria-hidden="true">« Anterior </span></a>`);
+
+      $('.pagination').append($previous);
+ 
+    }
+
+    if(numberOfRecords-begin > numberToShow){
+      
+      let $next = $(`<li class="page-item">
+      <a class="page-link" href="index.html?begin=${begin + numberToShow}&numberToShow=${numberToShow}"  aria-label="Next">
+      <span aria-hidden="true">Proxima »</span></a>
+      </li>`);
+
+      $('.pagination').append($next);
+ 
+    }
+
+    people = people.slice(begin, begin + numberToShow);
+
+    people.forEach(person => {
 
         let $personItem = createPersonItem(person);
         $('#people-table').append($personItem);
@@ -33,3 +78,6 @@ function updateView() {
 
 
 updateView();
+
+
+})
